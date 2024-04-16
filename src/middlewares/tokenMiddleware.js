a9e02621237baee4d44fa93141d37b5fed7aaf55
Alv4r0Dev13 <alvarorenancf00@gmail.com>
2024-04-16 15:29:11 -0300

    Arranging token middleware

--- src/middlewares/tokenMiddleware.js
+++ src/middlewares/tokenMiddleware.js
@@ -2,21 +2,23 @@ const jwt = require('jsonwebtoken');
 
 const isAuthenticated = async (req, res, next) => {
   const authToken = req.header('Authorization');
-  if (!authToken) return res.status(401).json({ error: 'Token invalido!.' });
+  if (!authToken)
+    return res.status(401).json({
+      errors: ['Login necessário!']
+    });
+
   const token = authToken.split(' ')[1];
-  if (token) {
-    try {
-      jwt.verify(token, process.env.SECRET_KEY);
-
-      return next();
-    } catch (e) {
-      return res.status(401).json({ error: 'Token invalido!.' });
-    }
-  } else {
-    res.status(401).json({ error: 'É preciso de um token de autorização!' });
+  if (token) try {
+    jwt.verify(token, process.env.SECRET_KEY);
+    return next();
+  } catch (e) {
+    return res.status(401).json({
+      errors: ['Usuário invalido! Tente fazer login novamente!']
+    });
   }
+  else return res.status(401).json({
+    errors: ['Login necessário!']
+  });
 };
 
-module.exports = {
-  isAuthenticated
-};
+module.exports = isAuthenticated;
