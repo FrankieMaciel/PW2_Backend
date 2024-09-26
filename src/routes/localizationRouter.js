73f67977cdf7ebe32b103303531bb200b3b27ce0
aaFranks <110301698+FrankieMaciel@users.noreply.github.com>
2024-09-26 00:46:13 -0300

    bug fixes

--- src/routes/localizationRouter.js
+++ src/routes/localizationRouter.js
@@ -7,7 +7,7 @@ const localizationController = require(path.resolve(__dirname, '..', 'controller
 const tokenMiddleware = require(path.resolve(__dirname, '..', 'middlewares', 'tokenMiddleware'));
 TM = tokenMiddleware.isAuthenticated;
 
-router.get('/user/:id', TM, localizationController.readByUser);
+router.get('/user/:id', localizationController.readByUser);
 router.post('/', TM, localizationController.create);
 router.put('/:id', TM, localizationController.update);
 router.delete('/:id', TM, localizationController.delete);
